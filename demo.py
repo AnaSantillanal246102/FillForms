007b1044cbb9aeffb129fd01e31d57d419a94aa5
Ana Santillan 246102 <al246102@alumnos.uacj.mx>
2026-06-10 09:45:07 -0600

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,12 @@
+import os
+import selenium.webdriver as webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
+
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36 Edg/148.0.0.0'
+edge_driver_path = os.path.join(os.getcwd(), 'msedgedriver.exe')
+edge_service = Service(edge_driver_path)
+edge_options = Options()
+edge_options.add_argument(f'user-agent={user_agent}')
+browser = webdriver.Edge(service=edge_service, options=edge_options)
+browser.get('https://developer.mozilla.org/es/docs/Web/HTTP/Reference/Headers/User-Agent')
